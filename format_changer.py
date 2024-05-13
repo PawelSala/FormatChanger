541a96c29353fad14cc11fb5270e733279bbaf12
PawelSala <pawel.sala01@wp.pl>
2024-05-13 20:01:51 +0200

    Parse arguments

---
+++ format_changer.py
@@ -0,0 +1,16 @@
+
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Data File Handler")
+    parser.add_argument("input_files", nargs=2, help="Input files and formats")
+    args = parser.parse_args()
+
+    input_files = args.input_files
+
+
+
+
+if __name__ == "__main__":
+    main()
